4c18d1b0b162c9408ada76c5bc836151ec3afe43
Jean-Philippe OURAGA <Jeanphilippeouraga52@gmail.com>
2023-04-04 21:09:47 +0000

    ajout de la page accueil(apres la connexion)

--- Contenu/accueil_login.php
+++ Contenu/accueil_login.php
@@ -4,9 +4,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/home_login.css">
     <title>Accueil</title>
 </head>
 <body>
-    
+    <!-- NAV VERTICALE -->
+    <div class="nav_vertical">
+            <p>😍❤️Lorem ipsum dolor sit amet consectetur adipisicing e
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem animi magnam natus consequatur veniam quasi ducimus impedit? Illum similique alias laboriosam aperiam perferendis tempora, temporibus a architecto ipsa, libero necessitatibus.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem animi magnam natus consequatur veniam quasi ducimus impedit? Illum similique alias laboriosam aperiam perferendis tempora, temporibus a architecto ipsa, libero necessitatibus.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem animi magnam natus consequatur veniam quasi ducimus impedit? Illum similique alias laboriosam aperiam perferendis tempora, temporibus a architecto ipsa, libero necessitatibus.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem animi magnam natus consequatur veniam quasi ducimus impedit? Illum similique alias laboriosam aperiam perferendis tempora, temporibus a architecto ipsa, libero necessitatibus.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem animi magnam natus consequatur veniam quasi ducimus impedit? Illum similique alias laboriosam aperiam perferendis tempora, temporibus a architecto ipsa, libero necessitatibus.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem animi magnam natus consequatur veniam quasi ducimus impedit? Illum similique alias laboriosam aperiam perferendis tempora, temporibus a architecto ipsa, libero necessitatibus.    
+            lit. Voluptatem animi magnam natus consequatur veniam quasi ducimus impedit? Illum similique alias laboriosam aperiam perferendis tempora, temporibus a architecto ipsa, libero necessitatibus.</p>
+    </div>
+
+    <!-- NAV Horizontale -->
+    <div class="nav_horizontal">
+        <nav class="navbar">
+
+        </nav>
+        <div class="contain">
+
+        </div>
+
+    </div>    
 </body>
 </html>
