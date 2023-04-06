e785ca8dcb1242a324e007b0e6cc31d0f024367b
Jean-Philippe OURAGA <Jeanphilippeouraga52@gmail.com>
2023-04-06 09:39:40 +0000

    mise a jour de la page d'apres connexion avec ajout de photo de profil et bouton deconnexion dans la nav horizontale

--- Contenu/accueil_login.php
+++ Contenu/accueil_login.php
@@ -5,7 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/home_login.css">
-    <title>Accueil</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
+    <title>Home</title>
 </head>
 <body>
     <!-- NAV VERTICALE -->
@@ -19,27 +20,58 @@
 
             <div class="scroll_menu">
                 <ul>
-                <a href="#" class="active">Dashboard</a>
-                <a href="" class='link'>Messagerie</a>
-                <a href="#">Répertoire</a>
-                <a href="#">Paramètre</a>
+                <div>
+                    <!-- <i class="fa-solid fa-chart-line"></i> -->
+                    <a href="#" class="active">Dashboard</a>   
+                </div>
+
+                <div>
+                <!-- <i class="fa-solid fa-chart-line"></i> -->
+                    <a class="link1" href="#">Messagerie</a>
+                        <div class="menu_messagerie">
+                            <a href="#">Nouveau SMS</a>
+                            <a href="#">Historique SMS</a>
+                         </div>
+                <div>
+                    <!-- <i class="fa-solid fa-chart-line"></i> -->
+                    <a class="link2" href="#">Répertoire</a>
+                        <div class="menu_repertoire">
+                            <a href="#">Contact</a>
+                            <a href="#">Liste Diffusion</a>
+                        </div>
+                <div>
+
+                <div>
+                    <!-- <i class="fa-solid fa-chart-line"></i> -->
+                    <a class="link3" href="#">Paramètre</a>
+                        <div class="menu_parametre">
+                            <a href="#">Profil</a>
+                            <a href="#">Achater Packs</a>
+                            <a href="#">Historique Achats</a>
+                            <a href="#">Contactez Support</a>
+                        </div>
+                </div>
                 </ul>
             </div>
-
-
-
-
     </div>
 
     <!-- NAV Horizontale -->
     <div class="nav_horizontal">
         <nav class="navbar">
-
+            <div class="session">
+                    <a href=""><img class="img_profile" src="../assets/image/profile_vide.jpg" alt=""></a>
+                    <a href=""><img class="img_deconnexion" src="../assets/image/deconnexion.jpg" alt=""></a>
+            </div>
+            <!--
+            <div class="info">
+                <p>Nombre SMS </p>
+                <a href="">Acheter Pack</a>  
+            </div>
+            -->
         </nav>
         <div class="contain">
 
         </div>
-
     </div>    
 </body>
 </html>
