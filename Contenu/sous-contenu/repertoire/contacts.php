24a6cead4df4bdf5402aa387a4c71df443a58eeb
Ahideon Mondesir <ahideonmondesir@gmail.com>
2023-04-17 09:46:21 +0000

    Update contacts.php
    ajout des includes des deux header à la page contact

---
+++ Contenu/sous-contenu/repertoire/contacts.php
@@ -0,0 +1,20 @@
+
+    <!-- Header -->
+
+    <?php include('../../nav/head.php');   ?>
+    
+    <title>envoyer SMS</title>
+    <!-- NAV VERTICALE -->
+    <?php
+        include('../../nav/nav_vertical.php');
+    ?>
+
+    <!-- NAV Horizontale -->
+    <div class="nav_horizontal">
+    <?php
+        include('../../nav/nav_horizontal.php');
+    ?> 
+  
+</body>
+</html>
+<script src="../../../assets/js/menu.js"></script>
