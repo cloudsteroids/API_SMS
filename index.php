fa3982ec8782435336f3d69c0bb98a1f4c2d91f0
Ahideon Mondesir <ahideonmondesir@gmail.com>
2023-04-27 15:26:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>CS'Texto</title>
+  <title>CS'Text</title>
 
   <!-- Favicons -->
   <link href="assets/img/favicon-16x16.png" rel="icon">
