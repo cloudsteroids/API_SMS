64379990ccf2362cf3c4ccaf4a83795a86178aca
Ahideon Mondesir <ahideonmondesir@gmail.com>
2023-05-02 17:25:30 +0100

    Update achat_pack.php

--- Contenu/sous-contenu/parametres/achat_pack.php
+++ Contenu/sous-contenu/parametres/achat_pack.php
@@ -17,7 +17,170 @@
         include('../../nav/nav_horizontal.php');
     ?>
         <div class="containn" id="containn">
-      
+            <div class="pagetitle">
+                <h1>ACHAT DE PACK SMS</h1>
+                <nav>
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="../dashboard.php">Dashboard</a></li>
+                        <li class="breadcrumb-item active">Historique des sms</li>
+                    </ol>
+                </nav>
+            </div><!-- End Page Title -->
+            <section class="section">
+                <div class="content">
+                    <div class="container-fluid">
+                        <div class="row portlet-row-fill-xl">
+
+                            <!---------------- Listes des packs ------------------------------->
+                            <div class="row">
+                                <div class="col-md-12">                                      
+                                    <div class="portlet">
+                                        <div class="portlet-header portlet-header-bordered">
+                                            <h3 class="portlet-title">Nos Packs</h3>
+                                        </div>
+                                        <div class="portlet-body">
+                                            <div class="row">                                                              
+                                                <div class="portlet mb-md-0">
+                                                    <div class="portlet-header portlet-header-bordered">
+                                                        <h3 class="portlet-title">Basic </h3>
+                                                    </div>
+                                                    <div class="portlet-body">
+                                                        <ul class="list-group">
+                                                            <li class="list-group-item text-center" style="size: 100px;">
+                                                                <strong><span class="badge border-primary border-1 text-primary badge-xl"> 100 sms</span></strong>
+                                                            </li>
+                                                            <li class="list-group-item"></li>
+                                                            <li class="list-group-item text-center">2,600 XOF</li>
+                                                            <li class="list-group-item"></li>
+                                                            <li class="list-group-item" style="background-color: #5e61ed;">
+                                                                <form action="" method="post">
+                                                                    <input type="hidden" name="" value="">                                                                
+                                                                    <input type="text" name="pack" value="2" style="display: none;">
+                                                                    <button type="submit" class="btn btn-primary">Acheté</button>
+                                                                </form>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                                <div class="portlet mb-md-0">
+                                                    <div class="portlet-header portlet-header-bordered">
+                                                        <h3 class="portlet-title">Medium </h3>
+                                                    </div>
+                                                    <div class="portlet-body">
+                                                        <ul class="list-group">
+                                                            <li class="list-group-item text-center" style="size: 100px;">
+                                                                <strong><span class="badge border-primary border-1 text-primary badge-xl"> 500 sms</span></strong>
+                                                            </li>
+                                                            <li class="list-group-item"></li>
+                                                            <li class="list-group-item text-center">12,500 XOF</li>
+                                                            <li class="list-group-item"></li>
+                                                            <li class="list-group-item" style="background-color: #5e61ed;">
+                                                                <form action="" method="post">
+                                                                    <input type="hidden" name="" value="">                                                                
+                                                                    <input type="text" name="pack" value="3" style="display: none;">
+                                                                    <button type="submit" class="btn btn-primary">Acheté</button>
+                                                                </form>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                                <div class="portlet mb-md-0">
+                                                    <div class="portlet-header portlet-header-bordered">
+                                                        <h3 class="portlet-title">Premium </h3>
+                                                    </div>
+                                                    <div class="portlet-body">
+                                                        <ul class="list-group">
+                                                            <li class="list-group-item text-center" style="size: 100px;">
+                                                                <strong><span class="badge border-primary border-1 text-primary badge-xl"> 1000 sms</span></strong>
+                                                            </li>
+                                                            <li class="list-group-item"></li>
+                                                            <li class="list-group-item text-center">24,000 XOF</li>
+                                                            <li class="list-group-item"></li>
+                                                            <li class="list-group-item" style="background-color: #5e61ed;">
+                                                                <form action="" method="post">
+                                                                    <input type="hidden" name="" value="">                                                                
+                                                                    <input type="text" name="pack" value="4" style="display: none;">
+                                                                    <button type="submit" class="btn btn-primary">Acheté</button>
+                                                                </form>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                                <div class="portlet mb-md-0">
+                                                    <div class="portlet-header portlet-header-bordered">
+                                                        <h3 class="portlet-title">Smart </h3>
+                                                    </div>
+                                                    <div class="portlet-body">
+                                                        <ul class="list-group">
+                                                            <li class="list-group-item text-center" style="size: 100px;">
+                                                                <strong><span class="badge border-primary border-1 text-primary badge-xl"> 5000 sms</span></strong>
+                                                            </li>
+                                                            <li class="list-group-item"></li>
+                                                            <li class="list-group-item text-center">115,000 XOF</li>
+                                                            <li class="list-group-item"></li>
+                                                            <li class="list-group-item" style="background-color: #5e61ed;">
+                                                                <form action="" method="post">
+                                                                    <input type="hidden" name="" value="">                                                                
+                                                                    <input type="text" name="pack" value="5" style="display: none;">
+                                                                    <button type="submit" class="btn btn-primary">Acheté</button>
+                                                                </form>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                                <div class="portlet mb-md-0">
+                                                    <div class="portlet-header portlet-header-bordered">
+                                                        <h3 class="portlet-title">Hyper </h3>
+                                                    </div>
+                                                    <div class="portlet-body">
+                                                        <ul class="list-group">
+                                                            <li class="list-group-item text-center" style="size: 100px;">
+                                                                <strong><span class="badge border-primary border-1 text-primary badge-xl"> 10000 sms</span></strong>
+                                                            </li>
+                                                            <li class="list-group-item"></li>
+                                                            <li class="list-group-item text-center">210,000 XOF</li>
+                                                            <li class="list-group-item"></li>
+                                                            <li class="list-group-item" style="background-color: #5e61ed;">
+                                                                <form action="" method="post">
+                                                                    <input type="hidden" name="" value="">                                                                
+                                                                    <input type="text" name="pack" value="6" style="display: none;">
+                                                                    <button type="submit" class="btn btn-primary">Acheté</button>
+                                                                </form>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                                <div class="portlet mb-md-0">
+                                                    <div class="portlet-header portlet-header-bordered">
+                                                        <h3 class="portlet-title">Pro </h3>
+                                                    </div>
+                                                    <div class="portlet-body">
+                                                        <ul class="list-group">
+                                                            <li class="list-group-item text-center" style="size: 100px;">
+                                                                <strong><span class="badge border-primary border-1 text-primary badge-xl"> 15000 sms</span></strong>
+                                                            </li>
+                                                            <li class="list-group-item"></li>
+                                                            <li class="list-group-item text-center">300,000 XOF</li>
+                                                            <li class="list-group-item"></li>
+                                                            <li class="list-group-item" style="background-color: #5e61ed;">
+                                                                <form action="" method="post">
+                                                                    <input type="hidden" name="_token" value="">                                                                
+                                                                    <input type="text" name="pack" value="7" style="display: none;">
+                                                                    <button type="submit" class="btn btn-primary">Acheté</button>
+                                                                </form>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>    
+                    </div>
+                </div>
+            </section>      
         </div>   
 </body>
 </html>
