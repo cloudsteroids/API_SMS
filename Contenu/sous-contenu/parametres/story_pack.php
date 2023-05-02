192049674a93fc55f3a4910c3b315d676c71362f
Ahideon Mondesir <ahideonmondesir@gmail.com>
2023-05-02 17:25:51 +0100

    Update story_pack.php

--- Contenu/sous-contenu/parametres/story_pack.php
+++ Contenu/sous-contenu/parametres/story_pack.php
@@ -17,9 +17,50 @@
         include('../../nav/nav_horizontal.php');
     ?>
         <div class="containn" id="containn">
-
-
-        </div>
-    </div>    
+            <div class="pagetitle">
+                <h1>HISTORIQUE DES PACKS SMS</h1>
+                <nav>
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="../dashboard.php">Dashboard</a></li>
+                        <li class="breadcrumb-item">Paramètre</li>
+                        <li class="breadcrumb-item active">Historique des Packs</li>
+                    </ol>
+                </nav>
+            </div><!-- End Page Title -->
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Pack acheté</h5>
+                    <!-- Liste des packs acheté -->
+                <div class="list-group">
+                  <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+                    <div class="d-flex w-100 justify-content-between">
+                      <h5 class="mb-1">Pack Mini</h5>
+                      <small>Il ya 3 jours</small>
+                    </div>
+                    <p class="mb-1">le pack mini vous donne droit à 50sms.</p>
+                    <small>Ce pack est illimité.</small>
+                  </a>
+                  <a href="#" class="list-group-item list-group-item-action">
+                    <div class="d-flex w-100 justify-content-between">
+                      <h5 class="mb-1">Pack Premium</h5>
+                      <small class="text-muted">Il ya 10 jours</small>
+                    </div>
+                    <p class="mb-1">le Pack premium vous donne droit à 1000sms.</p>
+                    <small class="text-muted">Ce pack est illimité puis vous donne droit à un conseil gratuit.</small>
+                  </a>
+                  <a href="#" class="list-group-item list-group-item-action">
+                    <div class="d-flex w-100 justify-content-between">
+                      <h5 class="mb-1">Pack Maxi</h5>
+                      <small class="text-muted">Il ya 50 jours</small>
+                    </div>
+                    <p class="mb-1">Le pack maxi vous donne droit à 500Sms.</p>
+                    <small class="text-muted">Ce pack est illimité.</small>
+                  </a>
+                </div><!-- End Liste des packs acheté -->
+              </div>
+            </div>
+            
+          </div>
+   
 </body>
 </html>
