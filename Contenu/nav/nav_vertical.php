78f9eb72cb8ffafa83144f0d5bf913a4f8a03d31
help <coulibalykorotoum0808@gmail.com>
2023-05-16 12:07:32 +0000

    amelioration de la nav vertical

--- Contenu/nav/nav_vertical.php
+++ Contenu/nav/nav_vertical.php
@@ -9,29 +9,29 @@
             <div class="scroll_menu">
                 <div>
                     <!-- <i class="fa-solid fa-chart-line"></i> -->
-                    <a href="../dashboard.php" class="active">Dashboard</a>   
+                    <a href="../dashboard.php" class="active"><i class="fa fa-list" aria-hidden="true"></i>  Dashboard</a>   
                 </div>
                 
                 <span class="container">
                     <div>
                     <!-- <i class="fa-solid fa-chart-line"></i> 
                 <i class="fa fa-caret-down"></i>-->
-                        <a class="messagerie" href="#">Messagerie </a>
+                        <a class="messagerie" href="#"><i class="fa fa-commenting" aria-hidden="true"></i> Messagerie </a>
                     </div>
                     <ul class="menu">
-                        <li><a href="../messagerie/new_sms.php">Nouveau SMS</a></li>
-                        <li><a href="../messagerie/story_sms.php">Historique SMS</a></li>
+                        <li><a href="../messagerie/new_sms.php"><i class="fa fa-paper-plane" aria-hidden="true"></i> Nouveau SMS</a></li>
+                        <li><a href="../messagerie/story_sms.php"><i class="fa fa-file-text" aria-hidden="true"></i> Historique SMS</a></li>
                     </ul>
                 </span>
                 
                 <span class="container">
                 <div>
                 <!-- <i class="fa-solid fa-chart-line"></i>-->
-                    <a class="repertoire" href="#">Répertoire</a> 
+                    <a class="repertoire" href="#"><i class="fa fa-address-book" aria-hidden="true"></i> Répertoire</a> 
                 </div>
                 <ul class="menu">
-                    <li><a href="../repertoire/contacts.php">Contact</a></li>
-                    <li><a href="../repertoire/liste_diffusion.php">Liste Diffusion</a></li>
+                    <li><a href="../repertoire/contacts.php"><i class="fa fa-mobile" aria-hidden="true"></i> Contact</a></li>
+                    <li><a href="../repertoire/liste_diffusion.php"><i class="fa fa-share-square" aria-hidden="true"></i> Liste Diffusion</a></li>
                 </ul>
                 </span>
 
@@ -39,13 +39,13 @@
                 <span class="container">
                 <div>
                     <!-- <i class="fa-solid fa-chart-line"></i> -->
-                    <a class="parametres" href="#">Paramètres</a> 
+                    <a class="parametres" href="#"><i class="fa fa-cogs" aria-hidden="true"></i> Paramètres</a> 
                 </div>
                 <ul class="menu">
-                    <li><a href="../parametres/profil.php">Profil</a></li>
-                    <li><a href="../parametres/achat_pack.php">Acheter Packs</a></li>
-                    <li><a href="../parametres/story_pack.php">Historique Achats</a></li>
-                    <li><a href="../parametres/support.php">Contactez Support</a></li> 
+                    <li><a href="../parametres/profil.php"><i class="fa  fa-user-md" aria-hidden="true"></i> Profil</a></li>
+                    <li><a href="../parametres/achat_pack.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Acheter Packs</a></li>
+                    <li><a href="../parametres/story_pack.php"><i class="fa fa-history" aria-hidden="true"></i> Historique Achats</a></li>
+                    <li><a href="../parametres/support.php"><i class="fa fa-ticket" aria-hidden="true"></i> Contactez Support</a></li> 
                 </ul>
                 </span>
             </div>
